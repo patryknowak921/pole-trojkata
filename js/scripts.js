90e5945fe7ae7e49218c4955e5e93f2e0de41bef
Patryk <patnow92@wp.pl>
2017-05-07 22:30:49 +0200

    sad

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var a = prompt ('Wpisz wartość a'), 
+	h = prompt ('Wpisz wartość h'),
+	triangleArea = a*h/2; // triangleArea ma wartość: a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
